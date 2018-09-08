7734c2116e6cbc86922d31de5fb9bd748f5633dd
zouxin <zouxin@iboxpay.com>
2018-09-08 17:54:28 +0800

    ⚡️ fix eslint error

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,19 @@
 module.exports = {
   outputDir: 'dist',
   assetsDir: 'static',
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "./node_modules/sass-bem/_bem.scss";`
+      }
+    }
+  },
   devServer: {
     proxy: {
       '/api': {
         target: 'https://cnodejs.org/api/v1',
         pathRewrite: { '^/api': '' },
         changeOrigin: true
-      }
-    }
-  },
-  css: {
-    loaderOptions: {
-      sass: {
-        data: `@import "./node_modules/sass-bem/_bem.scss";`
       }
     }
   }
