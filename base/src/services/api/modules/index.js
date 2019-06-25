390cb2b172595df22c775316e2f14d2618e529e6
zouxin <zouxin@iboxpay.com>
2019-06-25 12:46:49 +0800

    base - api 的自动引入

--- base/src/services/api/modules/index.js
+++ base/src/services/api/modules/index.js
@@ -1,5 +1,11 @@
-import a from './a'
-
-export default {
-  ...a
-}
+export default (() => {
+  const modulesContext = require.context('./', false, /(?<!index)\.js$/)
+  const chunks = modulesContext.keys().reduce((chunks, key) => {
+    return Object.assign(chunks, { [key.replace(/(^.*\/)|(\.js$)/g, '')]: modulesContext(key).default })
+  }, {})
+  const result = Object.keys(chunks).reduce((modules, key) => {
+    Object.assign(modules, chunks[key])
+    return modules
+  }, {})
+  return result
+})()
